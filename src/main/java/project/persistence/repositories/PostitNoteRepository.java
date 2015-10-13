d6510a32501d4d9e7c2ce41b2ae1b01114abf17f
Daniel Pall Johannsson <danpjoh@gmail.com>
2015-10-13 22:32:22 +0000

    Removed uneeded lines.

--- src/main/java/project/persistence/repositories/PostitNoteRepository.java
+++ src/main/java/project/persistence/repositories/PostitNoteRepository.java
@@ -2,19 +2,17 @@ package project.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import project.persistence.entities.PostitNote;
 
 import java.util.List;
 
 /**
- * By extending the {@link CrudRepository} we have access to powerful methods.
+ * By extending the {@link JpaRepository} we have access to powerful methods.
  * For detailed information, see:
  * http://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/CrudRepository.html
  * http://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html
  *
  */
-//public interface PostitNoteRepository extends CrudRepository<PostitNote, Long>{
 public interface PostitNoteRepository extends JpaRepository<PostitNote, Long> {
 
     PostitNote save(PostitNote postitNote);
